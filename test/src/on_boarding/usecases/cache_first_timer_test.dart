4ae1623385c94e81f3db6dc0b4b0841e512034f7
bentalla108 <tallambaye108@gmail.com>
2023-11-12 03:43:19 +0100

    OnBoarding page content

--- test/src/on_boarding/usecases/cache_first_timer_test.dart
+++ test/src/on_boarding/usecases/cache_first_timer_test.dart
@@ -1,6 +1,6 @@
 import 'package:course_app2/core/errors/failures.dart';
 import 'package:course_app2/src/on_boarding/domain/repos/on_boarding_repo.dart';
-import 'package:course_app2/src/on_boarding/usecases/cache_first_timer.dart';
+import 'package:course_app2/src/on_boarding/domain/usecases/cache_first_timer.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
--- test/src/on_boarding/usecases/check_if_user_is_first_timer_test.dart
+++ test/src/on_boarding/usecases/check_if_user_is_first_timer_test.dart
@@ -1,5 +1,5 @@
 import 'package:course_app2/src/on_boarding/domain/repos/on_boarding_repo.dart';
-import 'package:course_app2/src/on_boarding/usecases/check_if_user_is_first_timer.dart';
+import 'package:course_app2/src/on_boarding/domain/usecases/check_if_user_is_first_timer.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
